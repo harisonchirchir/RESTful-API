01f16eec43927be69097368168427dd402d754ae
harison kimutai chirchir <harisonchirchir25@gmail.com>
2026-06-16 15:11:32 +0300

    Fix serializer field name mismatch and test payload key

--- books/serializers.py
+++ books/serializers.py
@@ -4,21 +4,17 @@ from .models import Book, Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['id', 'name', 'bio']
+        fields = ['id', 'name', 'biography']
 
 class BookSerializer(serializers.ModelSerializer):
     author_id = serializers.PrimaryKeyRelatedField(
-        
-        queryset=Author.objects.all(),
-        source='author',
+        queryset=Author.objects.all(), 
+        source='author', 
         write_only=True
     )
-        
-        
     author = AuthorSerializer(read_only=True)
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author_id', 'is_read', 'owner', 'author']
-        
+        fields = ['id', 'title', 'is_read', 'owner', 'author_id', 'author']
         read_only_fields = ['owner']
--- books/tests.py
+++ books/tests.py
@@ -8,7 +8,7 @@ class BookAPITestCase(APITestCase):
     
     def setUp(self):
         self.user = User.objects.create_user(username='desmond', password='2108abcd')
-        self.author = Author.objects.create(name='J.R.R. Tolkien', bio='English writer, poet, philologist, and academic.')
+        self.author = Author.objects.create(name='J.R.R. Tolkien', biography='English writer, poet, philologist, and academic.')
         self.url = reverse('book-list-create')
 
     def test_get_books_unauthorized_return_401(self):
@@ -25,9 +25,10 @@ class BookAPITestCase(APITestCase):
     def test_create_book_authenticated_returns_201(self):
         """Ensure logged in users can create a book and receive a 201 status code."""
         self.client.force_authenticate(user=self.user)
+
         data = {
             "title": "The Silmarillion", 
-            "author": self.author.id, 
+            "author_id": self.author.id, 
             "is_read": False
         }
         response = self.client.post(self.url, data, format='json')
@@ -43,6 +44,5 @@ class BookAPITestCase(APITestCase):
         response = self.client.get(self.url, {'is_read': 'true'})
         
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['title'], "Read Book")
